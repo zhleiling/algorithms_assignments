f8661aa591725f3233ce3de891935d19887c7a13
zhleiling <117630566@qq.com>
2014-09-19 12:00:24 +0800

    First java code commit; for test

---
+++ queues/algorithms_assignments/Deque.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package algorithms_assignments;
+
+import java.util.Iterator;
+
+/**
+ * @author zhanglei01
+ * @param <T>
+ *
+ */
+public class Deque<T> implements Iterable<T> {
+
+	/**
+	 * 
+	 */
+	public Deque() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Iterator<T> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
